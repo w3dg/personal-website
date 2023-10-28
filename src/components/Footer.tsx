58ae889524ea7ae8a9ffe57ff5533974a0709236
w3dg <51162936+w3dg@users.noreply.github.com>
2023-10-28 13:06:21 +0530

    Footer Fix hover state for icons and links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,6 @@
-import { TbBrandGithub, TbMailForward } from "react-icons/tb";
+import { IconContext } from "react-icons";
+import { LuMail, LuGithub, LuContact } from "react-icons/lu";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -8,21 +10,23 @@ export const Footer = () => {
         <p>made with &lt;3 and coffee</p>
         <p>&copy;{currentYear} DG Media All Rights Reserved</p>
         <ul className="flex gap-2">
-          <li>
-            <a href="mailto:w3dg@duck.com">
-              <TbMailForward></TbMailForward>
-            </a>
-          </li>
-          <li>
-            <a href="https://dgsh.vercel.app/gh">
-              <TbBrandGithub></TbBrandGithub>
-            </a>
-          </li>
-          <li>
-            <a href="mailto:w3dg@duck.com">
-              <TbMailForward></TbMailForward>
-            </a>
-          </li>
+          <IconContext.Provider value={{ className: "text-neutral-500 hover:text-neutral-100" }}>
+            <li>
+              <a href="mailto:w3dg@duck.com">
+                <LuMail></LuMail>
+              </a>
+            </li>
+            <li>
+              <a href="https://dgsh.vercel.app/gh">
+                <LuGithub></LuGithub>
+              </a>
+            </li>
+            <li>
+              <Link to={"/contact"}>
+                <LuContact></LuContact>
+              </Link>
+            </li>
+          </IconContext.Provider>
         </ul>
       </div>
     </footer>
