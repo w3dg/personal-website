f547fe6755b37bc9eeae4667f1a20ed1f76f5fe5
w3dg <51162936+w3dg@users.noreply.github.com>
2023-10-28 12:59:22 +0530

    add shadow to navbar when past threshold

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+  const [scrolledPast, setScrolledPast] = useState(false);
+  const threshold = 60;
+
+  const handleScroll = () => {
+    if (window.scrollY > threshold) {
+      setScrolledPast(true);
+    } else {
+      setScrolledPast(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <nav className="sticky inset-x-0 top-0 z-10 flex justify-between px-4 py-3 border-b-2 lg:px-6 border-neutral-50/10 bg-neutral-950/70 text-neutral-200 backdrop-blur-md lg:py-4">
+      <nav
+        className={
+          "sticky inset-x-0 top-0 z-10 flex justify-between px-4 py-3 lg:px-6 bg-neutral-950/70 text-neutral-200 backdrop-blur-md lg:py-4" + (scrolledPast ? " shadow-lg shadow-neutral-900" : "")
+        }
+      >
         <Link to="/">DG</Link>
         <ul className="flex space-x-4">
           <Link to="/">Home</Link>
