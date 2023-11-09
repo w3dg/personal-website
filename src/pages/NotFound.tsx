03c1c20d0dfb492ae49cc4aa078b9f89221108cc
w3dg <51162936+w3dg@users.noreply.github.com>
2023-11-10 01:36:50 +0530

    update not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+import { Navbar } from "../components/Navbar";
+
 export const NotFound = () => {
   return (
-    <div>
-      <h1>Oops! The page is not found</h1>
-    </div>
+    <>
+      <Navbar></Navbar>
+      <div className="grid gap-8 mt-48 text-center place-content-center">
+        <img src="/not-found.svg" alt="Not found" className="w-64" />
+        <h1>Oops! The page is not found</h1>
+        <p>
+          Go{" "}
+          <Link className="underline hover:text-neutral-400" to={"/"}>
+            home
+          </Link>
+        </p>
+      </div>
+    </>
   );
 };
