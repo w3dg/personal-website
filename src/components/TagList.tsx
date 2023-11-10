a6723591558e7bbba5abe0f1d227c2dccad4a160
w3dg <51162936+w3dg@users.noreply.github.com>
2023-11-10 16:26:37 +0530

    Put Key Prop for tags

--- src/components/TagList.tsx
+++ src/components/TagList.tsx
@@ -8,7 +8,7 @@ export const TagList = ({ tags }: TagListProps) => {
   return (
     <div className="flex gap-2 mt-1">
       {tags.map((tag) => (
-        <Tag value={tag} />
+        <Tag value={tag} key={tag} />
       ))}
     </div>
   );
